441092f1717779749ae162ebcf8424bb779fbf90
Erfik8 <31968604+Erfik8@users.noreply.github.com>
2023-06-11 10:14:04 +0200

    Update Town.java

--- src/main/java/efs/task/collections/entity/Town.java
+++ src/main/java/efs/task/collections/entity/Town.java
@@ -22,17 +22,27 @@ public class Town {
     //TODO implementacja metody equal porównująca obiekty Town na podstawie tylko townName.
     @Override
     public boolean equals(Object o) {
-        return true;
+        if (this == o) return true;
+        else if (o == null || getClass() != o.getClass()) return false;
+        else {
+            Town town = (Town) o;
+            return townName.equals(town.townName);
+        }
     }
 
     //TODO implementacja metody equal biorąca pod uwagę tylko townName.
     @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(townName);
     }
 
     @Override
     public String toString() {
         return "Miasto :" + townName;
+    }
+
+    public int compareTo(Object town) {
+        Town otherTown = (Town) town;
+        return this.getTownName().compareTo(otherTown.getTownName());
     }
 }
